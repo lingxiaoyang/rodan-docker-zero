353e4580a6933a9debae5eb26c717b5222a28a65
Ling-Xiao Yang <ling@theyang.ca>
2020-01-03 19:37:29 -0500

    WIP: fix prod setup

---
+++ rodan/dev_settings/prod_settings.py
@@ -0,0 +1,40 @@
+from mirrored_rodan_settings import *  # noqa
+
+
+TEST_JOB_PACKAGES = [
+    "rodan.jobs.helloworld",
+]
+
+# We won't install gm or kdu_compress in dev environment
+ENABLE_DIVA = False
+
+
+# STOP: don't edit further
+##############################
+
+# Append TEST_JOB_PACKAGES to RODAN_JOB_PACKAGES
+for job_package in TEST_JOB_PACKAGES:
+    if job_package not in RODAN_JOB_PACKAGES:
+        RODAN_JOB_PACKAGES.append(job_package)
+
+# Monkey-patch django.setup() so that we won't run into hanging issues with
+# the setup() call in rodan/__init__.py => rodan/celery.py
+import django  # noqa
+import traceback  # noqa
+
+_orig_django_setup = django.setup
+
+
+def _setup_except_celery(*a, **k):
+    """monkey-patch"""
+    stack = traceback.extract_stack()
+    if len(stack) >= 2 and stack[-2][0] == '/srv/Rodan/rodan/celery.py':
+        print("MONKEY-PATCH: called from rodan/celery.py, do nothing")
+        return None
+    else:
+        return _orig_django_setup(*a, **k)
+
+
+# Avoid monkey-patching more than once
+if django.setup.__doc__ != 'monkey-patch':
+    django.setup = _setup_except_celery
